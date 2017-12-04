12531c6b8056974b113f75679178afd6f57c6fd7
Arjan Einbu <arjan@einbu.no>
2017-12-04 22:03:59 +0100

    luke 4

---
+++ luke4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace luke4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var filename = @"ordliste.txt";
+			using (var sr = new StreamReader(filename))
+			{
+				var count = 0;
+				var palindromeCount = 0;
+				while (!sr.EndOfStream)
+				{
+					var word = sr.ReadLine().Replace("-", "");
+
+					if (IsPalindrome(word))
+					{
+						// Console.WriteLine(word);
+						palindromeCount++;
+						continue;
+					}
+
+					if(HasPalindromeAnagram(word))
+					{
+						count++;
+						// Console.WriteLine(word);
+					}
+				}
+				Console.WriteLine($"total: {count + palindromeCount}, palindromes: {palindromeCount}, palindrome anagrams: {count}");
+			}
+
+			Console.WriteLine("\nDone!");
+		}
+
+        private static bool HasPalindromeAnagram(string word)
+        {
+			return word.GroupBy(c => c)
+				.OrderBy(g => g.Count())
+				.Skip(1)
+				.All(c => (c.Count() & 1) == 0);
+        }
+
+        private static bool IsPalindrome(string word)
+		{
+			if (word.First() == word.Last())
+			{
+				var numberOfLetters = word.Length - 2;
+				return numberOfLetters > 0
+					? IsPalindrome(word.Substring(1, numberOfLetters))
+					: true;
+			}
+
+			return false;
+		}
+	}
+}
