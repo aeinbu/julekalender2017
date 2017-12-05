0e0d7935f2c1a799d6a39baa5d5e4bec64ea1727
Arjan Einbu <arjan@einbu.no>
2017-12-05 19:31:04 +0100

    luke 5

---
+++ luke5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace luke5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 1000000;
+            var numbers = new List<int>(){1, 2, 2};            
+            var nextNum = 3;
+
+            while(numbers.Count < count)
+            {
+                numbers.AddRange(Enumerable.Repeat(nextNum, numbers[nextNum-1]));
+                nextNum++;
+            }
+
+            System.Console.WriteLine(numbers.Take(count).Sum(number => (long)number));
+
+            Console.WriteLine("\nDone!");
+        }
+
+    }
+}
