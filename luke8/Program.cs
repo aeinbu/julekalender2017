f99d0224879b6fb4ee6a6ec7ccdf5cccd51b7c73
Arjan Einbu <arjan@einbu.no>
2017-12-09 01:46:08 +0100

    luke 8

---
+++ luke8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace luke8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw = Stopwatch.StartNew();
+			long sum = 0;
+			for (int i = 0; i <= 10000000; i++)
+			{
+				if(GetLastInSequence(i) == 1)
+				{
+					sum += i;
+				}
+			}
+			Console.WriteLine(sw.Elapsed);
+			Console.WriteLine(sum);
+
+			Console.WriteLine("\nDone!");
+		}
+
+		private static int GetLastInSequence(int v)
+		{
+			var parts = GetDigits(v);
+			var nextSum = parts.Select(part => part*part).Sum(partSquared => partSquared);
+			if(new[]{0, 1, 4}.Contains(nextSum))
+			{
+				return nextSum;
+			}
+			// System.Console.WriteLine(nextSum);
+			return GetLastInSequence(nextSum);
+		}
+
+		private static List<int> GetDigits(int v)
+		{
+			if(v >= 10)
+			{
+				var ret = new List<int>{v % 10};
+				ret.AddRange(GetDigits(v / 10));
+				return ret;
+			}
+			return new List<int>{v};
+		}
+	}
+}
